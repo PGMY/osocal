96994deb90d5fb0147c209c4cf499afa527a1c7c
PGMY <pgmyjp@gmail.com>
2016-01-28 14:06:08 +0900

    change sample

--- js/site.js
+++ js/site.js
@@ -7,7 +7,7 @@ $( function() {
   var nextMonth    = moment().add('month', 1).format('YYYY-MM');
 
   var events = [
-    { date: currentMonth + '-' + '10', title: 'Persian Kitten Auction', location: 'Center for Beautiful Cats' },
+    { date: currentMonth + '-' + '10', title: 'Persian Kitten Auction', location: '<a href="#">Center for Beautiful Cats</a>aa' },
     { date: currentMonth + '-' + '19', title: 'Cat Frisbee', location: 'Jefferson Park' },
     { date: currentMonth + '-' + '23', title: 'Kitten Demonstration', location: 'Center for Beautiful Cats' },
     { date: nextMonth + '-' + '07',    title: 'Small Cat Photo Session', location: 'Center for Cat Photography' }
